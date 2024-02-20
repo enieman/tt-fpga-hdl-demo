ee634e42ab6ad7a9002733d64932756e11a3da20
enieman <159297950+enieman@users.noreply.github.com>
2024-02-20 16:26:04 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -42,7 +42,7 @@ endmodule
 // The Tiny Tapeout module
 // =======================
 
-module tt_um_enieman (
+module tt_um_template (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
        // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
